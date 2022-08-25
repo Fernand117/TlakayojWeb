fd70f28789aeebbc6f2994b456aa835eb2e53a9a
Fernand117 <fernando_rendel892@hotmail.com>
2022-08-24 21:29:47 -0500

    LFLL-COMPONENTES-CREATE
    Se creó el componente para el menú de administración y el pie de página

--- Tlakayoj/src/app/app.module.ts
+++ Tlakayoj/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PanelComponent } from './pages/panel/panel.component';
+import { MenuadminComponent } from './components/menuadmin/menuadmin.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PanelComponent
+    PanelComponent,
+    MenuadminComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
